a8f5fe0b152a66950f567cb69a76ca1d4b009686
bonk-dev <github.p0j6t@passinbox.com>
2024-05-18 16:12:22 +0200

    Wrap the inputs in a <form>

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -52,33 +52,37 @@ const Login = () => {
                 <h1 className={'text-3xl'}>XEON PWM Login</h1>
             </header>
 
-            <Input
-                isRequired
-                type={'text'}
-                label={'Username'}
-                className={'max-w-xs mb-3'}
-                value={username}
-                onValueChange={setUsername}/>
-            <Input
-                isRequired
-                type={'password'}
-                label={'Password'}
-                className={'max-w-xs mb-3'}
-                value={password}
-                onValueChange={setPassword}/>
+            <form className={'flex flex-col justify-center items-center w-full'}>
+                <Input
+                    isRequired
+                    type={'text'}
+                    label={'Username'}
+                    className={'max-w-xs mb-3'}
+                    value={username}
+                    onValueChange={setUsername}
+                    autoComplete={'username'}/>
+                <Input
+                    isRequired
+                    type={'password'}
+                    label={'Password'}
+                    className={'max-w-xs mb-3'}
+                    value={password}
+                    onValueChange={setPassword}
+                    autoComplete={'current-password'}/>
 
-            {error != null ? (
-                <p className={'text-danger mb-3'}>{error}</p>
-            ) : null}
+                {error != null ? (
+                    <p className={'text-danger mb-3'}>{error}</p>
+                ) : null}
 
-            <Button
-                color={'primary'}
-                variant={'ghost'}
-                startContent={<PopiconsLockDuotone/>}
-                onClick={handleOnLoginClick}
-                isLoading={isLoading}>
-                Login
-            </Button>
+                <Button
+                    color={'primary'}
+                    variant={'ghost'}
+                    startContent={<PopiconsLockDuotone/>}
+                    onClick={handleOnLoginClick}
+                    isLoading={isLoading}>
+                    Login
+                </Button>
+            </form>
         </article>
     );
 };
